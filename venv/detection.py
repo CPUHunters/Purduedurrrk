acb8d2585fe32aec1d08763f16eb02920761d831
jeonhs1202 <jeonhs1202@gmail.com>
2018-12-30 21:35:20 +0900

    add sound

--- venv/detection.py
+++ venv/detection.py
@@ -6,16 +6,22 @@ import os
 import retrain_run
 import shutil
 import time
-START=175
-END=181
+import pygame
+
+START=130
+END=140
+
+
 
 def bgSubMOG2(cap):
     fgbg = cv.createBackgroundSubtractorMOG2(history=500, varThreshold=500, detectShadows=0)
+    pygame.init()
+    pygame.mixer.music.load('Hawk.mp3')
+    count = 1                       #count는 프레임 번호
+    while (count<(END+20)):
 
-    count = 1
-    while (count<(END+1)):
-        if count==START:
-            start_time=time.time()
+        # if count==START:            #시작 순간부터 시간 측정
+        #     start_time=time.time()
 
         ret, frame = cap.read()
         _, original = cap.read()
@@ -26,14 +32,14 @@ def bgSubMOG2(cap):
         else:
             continue
 
-        lower_red = np.array([90, 0, 200])
-        upper_red = np.array([255, 255, 255])
-        mask = cv.inRange(hsv, lower_red, upper_red)
-        fmask = cv.inRange(hsv, lower_red, upper_red)
-        fmask = cv.medianBlur(fmask, 3)
+        lower_red = np.array([90, 0, 200])          #나뭇잎 컬러의 하위영역 설정
+        upper_red = np.array([255, 255, 255])       #나뭇잎 컬러의 상위영역 설정
+        mask = cv.inRange(hsv, lower_red, upper_red)        #mask에 색상영역 추출
+        fmask = cv.inRange(hsv, lower_red, upper_red)       #fmask에 색상영역 추출(미디안 필터를 적용하기 위해 따로 적용)
+        fmask = cv.medianBlur(fmask, 3)                     #median 필터를 fmask에 적용
 
-        res = cv.bitwise_and(frame, frame, mask=mask)
-        fres = cv.bitwise_and(frame, frame, mask=fmask)
+        res = cv.bitwise_and(frame, frame, mask=mask)       #나뭇잎 영역을 제외한 부분 추출
+        fres = cv.bitwise_and(frame, frame, mask=fmask)     #미디안 필터를 적용한 나뭇잎 영역을 제외한 부분 추출
 
         frame = cv.resize(frame, (640, 480), interpolation=cv.INTER_LINEAR)
         original = cv.resize(original, (640, 480), interpolation=cv.INTER_LINEAR)
@@ -51,7 +57,7 @@ def bgSubMOG2(cap):
         nlabels, labels, stats, centroids = cv.connectedComponentsWithStats(fgmaskres)
         str = 0
         if count >=START and count<END:
-            if not (os.path.isdir("./test/%s" % count)):
+            if not (os.path.isdir("./test/%s" % count)):            #해당 프레임에 대한 폴더 생성
                 os.makedirs(os.path.join("./test/%s" % count))
         for index, centroid in enumerate(centroids):
             if stats[index][0] == 0 and stats[index][1] == 0:
@@ -65,28 +71,33 @@ def bgSubMOG2(cap):
                     if (x-20) > 0 and (y-20) > 0:
                         imgGrop = test[y - 20:y + height + 20, x - 20:x + width + 20]
                         cv.imwrite("./test/%s/%d.jpg" % (count,str), imgGrop)
-                        #cv.imwrite("./test1/%d-%d.jpg" % (count, str), imgGrop)
                         str = str + 1
 
                 cv.circle(original, (centerX, centerY), 1, (0, 255, 0), 2)
                 cv.rectangle(original, (x, y), (x + width, y + height), (0, 0, 255))
-        if count >=START and count<END:
-            retrain_run.run_inference_on_image(count)
-        if count==END:
-            end_time=time.time()
-        #shutil.rmtree('./test')
-        #cv.imshow('frame_res', fgmaskres)  # Green deleted
-        #cv.imshow('original', original)
-        #print(count)
+        if count>= START and count < END:
+            sound=retrain_run.run_inference_on_image(count)
+            if sound ==1 and (not pygame.mixer.music.get_busy()):
+                pygame.mixer.music.play()
+
+
+        # if count==END:                      #시간 측정 완료
+        #     end_time=time.time()
+
         count = count + 1
+
+
+        #cv.imshow('frame_res', fgmaskres)  # Green deleted
+        cv.imshow('original', original)
         #cv.imshow('frame_original', fgmaskfra) # BS original
         #cv.imshow('res',res)
-        #cv.imshow('frame_filter', fgmaskfres) # Median filtered green deleted
+        cv.imshow('frame_filter', fgmaskfres) # Median filtered green deleted
+
         k = cv.waitKey(30) & 0xff
         if k == 27:
             break
-    print("--- %.6s seconds ---"%(end_time-start_time))
+
+    # print("--- %.6s seconds ---"%(end_time-start_time))     #소요 시간 출력
 
     cap.release()
-    # out.release()
     cv.destroyAllWindows()
--- venv/retrain_run.py
+++ venv/retrain_run.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 import tensorflow as tf
 
-
+import time
 
 #imagePath = '/tmp/test.jpg'                                      # 추론을 진행할 이미지 경로
 modelFullPath = './tmp/output_graph.pb'                                      # 읽어들일 graph 파일 경로
@@ -27,12 +27,15 @@ def run_inference_on_image(count):
     file_list = os.listdir(path_dir)
     file_list.sort()
 
-    file=open("./result.txt","a")
-    answer = None
-  #  if not tf.gfile.Exists(imagePath):
-   #     tf.logging.fatal('File does not exist %s', imagePath)
-    #    return answer
-    #print(file_list)
+    #file=open("./result.txt","a")
+    answer = 0
+
+    # if not tf.gfile.Exists(imagePath):
+    #     tf.logging.fatal('File does not exist %s', imagePath)
+    #     return answer
+    # print(file_list)
+
+
     for item in file_list:
         file_path = ("./test/%s/"%abc)+item
         #print(file_path)
@@ -50,7 +53,7 @@ def run_inference_on_image(count):
                                    {'DecodeJpeg/contents:0': image_data})
             predictions = np.squeeze(predictions)
 
-            #print('< %s >' % item)
+            print('< In %s th frame, %s image is being classified. >' % (abc,item))
 
             top_k = predictions.argsort()[-5:][::-1]  # 가장 높은 확률을 가진 5개(top 5)의 예측값(predictions)을 얻는다.
             f = open(labelsFullPath, 'rb')
@@ -60,14 +63,17 @@ def run_inference_on_image(count):
             for node_id in top_k:
                 human_string = labels[node_id]
                 score = predictions[node_id]
-                #print('\'%s\' (score = %.5f) %d' % (human_string[2:-3], score,order))
+                # print('\'%s\' (score = %.5f) %d' % (human_string[2:-3], score,order))
                 order = order+1
                 if ("bird" in human_string) and (order == 2) :
-                    data=("\'%s-%s\'  1\n" % (abc,item))
-                elif (order == 2):
-                    data=("\'%s-%s\'  0\n" % (abc,item))
-            file.write(data)
-            answer = labels[top_k[0]]
+                    #data=("\'%s-%s\'  1\n" % (abc,item))
+                    print("BIRD DETECTED!!!")
+                    answer = 1
+                    return answer
+                # elif (order == 2):
+                #     print("not detect")
+                    #data=("\'%s-%s\'  0\n" % (abc,item))
+            #file.write(data)
             f.close()
     return answer
 
